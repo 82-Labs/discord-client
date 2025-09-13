936ea597341e89652908aecffcdc1fe97c82740f
Minyeong Jeong <myjeong19@naver.com>
2025-09-13 11:56:07 +0900

    feat(channels): 헤더 및 드롭다운, 탭 구현 (#5)

---
+++ src/components/channels/header/ui/channels-header.tsx
@@ -0,0 +1,55 @@
+import { Link } from "react-router";
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from "~/shared/ui/dropdown-menu";
+import { Tooltip, TooltipTrigger, TooltipContent } from "~shared/ui/tooltip";
+
+import { SendMailBox } from "./send-mail-box";
+import { FriendsIcon } from "./icons/friends-icon";
+import { MailBoxIcon } from "./icons/mail-box-icon";
+import { SupportIcon } from "./icons/support-icon";
+
+export default function ChannelsHeader() {
+  return (
+    <header className="relative flex items-center justify-center bg-discord-black text-sm h-8 text-gray-300/80">
+      <div className="flex items-center gap-2">
+        <FriendsIcon className="w-4 h-4" />
+        <span>친구</span>
+      </div>
+
+      <div className="absolute right-3 flex items-center gap-5 z-1 w-20">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="w-9 h-6 outline-none cursor-pointer hover:text-white transition-all delay-200">
+            <Tooltip>
+              <TooltipTrigger>
+                <MailBoxIcon className="w-full h-full" />
+              </TooltipTrigger>
+              <TooltipContent className="p-2 text-sm bg-discord-gray h-full border-none **:*:fill-discord-gray  **:*:bg-discord-gray">
+                <span>받은 편지함</span>
+              </TooltipContent>
+            </Tooltip>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            side="bottom"
+            sideOffset={5}
+            align="end"
+            alignOffset={0}
+            className="bg-discord-gray h-full border-none"
+          >
+            <SendMailBox />
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <Link
+          to="https://support.discord.com/hc/ko"
+          className="flex items-center gap-2 w-5 h-5"
+        >
+          <SupportIcon className="w-full h-full" />
+        </Link>
+      </div>
+    </header>
+  );
+}
